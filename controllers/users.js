ad8af7540d4d755e791a028fb3a40e094c5fb756
Miryam Yanit Baranda Moreno <miryamybm@gmail.com>
2023-10-31 01:59:48 -0600

    Instalación y configuración del proyecto y su conexión a la BD
    Cree el proyecto, hice los archivos y carpetas necesarias, también instale los paquetes y dependencias que se ocuparán, finalmente realice la configuración a la conexión de la base de datos.

---
+++ controllers/users.js
@@ -0,0 +1,36 @@
+const { request, response } = require('express');
+const usersModel = require('../models/users')
+const pool = require('../db');
+
+/**
+ * ENDPOINT READ
+ * para listar toda la informacion
+ */
+
+
+/**
+ * ENDPOINT READ
+ * para listar informacion en base a un criterio
+ */
+
+
+/**
+ * ENDPOINT CREATE
+ * para agregar un nuevo registro
+ */
+
+
+
+/**
+ * ENDPOINT UPDATE
+ * para actualizar o modificar datos
+ */
+
+
+
+/**
+ * ENDPOINT DELETE
+ * para eliminar registro
+ */
+
+
